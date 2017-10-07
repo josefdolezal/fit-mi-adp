1bedd23a54e235b7417b6d00c1410762d447503a
Josef Dolezal <pepik.ml@gmail.com>
2017-10-07 21:03:11 +0200

    Add image assets

--- mvc-shooter/Generated/Images.swift
+++ mvc-shooter/Generated/Images.swift
@@ -1,3 +1,100 @@
 // Generated using SwiftGen, by O.Halligon — https://github.com/SwiftGen/SwiftGen
 
-// No assets found
+#if os(OSX)
+  import AppKit.NSImage
+  typealias AssetColorTypeAlias = NSColor
+  typealias Image = NSImage
+#elseif os(iOS) || os(tvOS) || os(watchOS)
+  import UIKit.UIImage
+  typealias AssetColorTypeAlias = UIColor
+  typealias Image = UIImage
+#endif
+
+// swiftlint:disable file_length
+
+@available(*, deprecated, renamed: "ImageAsset")
+typealias AssetType = ImageAsset
+
+struct ImageAsset {
+  fileprivate var name: String
+
+  var image: Image {
+    let bundle = Bundle(for: BundleToken.self)
+    #if os(iOS) || os(tvOS)
+    let image = Image(named: name, in: bundle, compatibleWith: nil)
+    #elseif os(OSX)
+    let image = bundle.image(forResource: NSImage.Name(name))
+    #elseif os(watchOS)
+    let image = Image(named: name)
+    #endif
+    guard let result = image else { fatalError("Unable to load image named \(name).") }
+    return result
+  }
+}
+
+struct ColorAsset {
+  fileprivate var name: String
+
+  @available(iOS 11.0, tvOS 11.0, watchOS 4.0, OSX 10.13, *)
+  var color: AssetColorTypeAlias {
+    return AssetColorTypeAlias(asset: self)
+  }
+}
+
+// swiftlint:disable identifier_name line_length nesting type_body_length type_name
+enum Asset {
+  static let bird = ImageAsset(name: "bird")
+  static let cannon = ImageAsset(name: "cannon")
+  static let piggyDamaged = ImageAsset(name: "piggy-damaged")
+  static let piggyHappy = ImageAsset(name: "piggy-happy")
+  static let piggyKo = ImageAsset(name: "piggy-ko")
+  static let piggyOk = ImageAsset(name: "piggy-ok")
+
+  // swiftlint:disable trailing_comma
+  static let allColors: [ColorAsset] = [
+  ]
+  static let allImages: [ImageAsset] = [
+    bird,
+    cannon,
+    piggyDamaged,
+    piggyHappy,
+    piggyKo,
+    piggyOk,
+  ]
+  // swiftlint:enable trailing_comma
+  @available(*, deprecated, renamed: "allImages")
+  static let allValues: [AssetType] = allImages
+}
+// swiftlint:enable identifier_name line_length nesting type_body_length type_name
+
+extension Image {
+  @available(iOS 1.0, tvOS 1.0, watchOS 1.0, *)
+  @available(OSX, deprecated,
+    message: "This initializer is unsafe on macOS, please use the ImageAsset.image property")
+  convenience init!(asset: ImageAsset) {
+    #if os(iOS) || os(tvOS)
+    let bundle = Bundle(for: BundleToken.self)
+    self.init(named: asset.name, in: bundle, compatibleWith: nil)
+    #elseif os(OSX)
+    self.init(named: NSImage.Name(asset.name))
+    #elseif os(watchOS)
+    self.init(named: asset.name)
+    #endif
+  }
+}
+
+extension AssetColorTypeAlias {
+  @available(iOS 11.0, tvOS 11.0, watchOS 4.0, OSX 10.13, *)
+  convenience init!(asset: ColorAsset) {
+    let bundle = Bundle(for: BundleToken.self)
+    #if os(iOS) || os(tvOS)
+    self.init(named: asset.name, in: bundle, compatibleWith: nil)
+    #elseif os(OSX)
+    self.init(named: NSColor.Name(asset.name), bundle: bundle)
+    #elseif os(watchOS)
+    self.init(named: asset.name)
+    #endif
+  }
+}
+
+private final class BundleToken {}
