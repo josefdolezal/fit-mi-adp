06f6969d36abf2a5d38ee05477918aae6d5364d0
Josef Dolezal <pepik.ml@gmail.com>
2017-11-26 14:01:44 +0100

    Create Location to track 2D location of objects

---
+++ mvc-shooter/Model/Location.swift
@@ -0,0 +1,18 @@
+//
+//  Location.swift
+//  mvc-shooter
+//
+//  Created by Josef Dolezal on 26/11/2017.
+//  Copyright © 2017 Josef Dolezal. All rights reserved.
+//
+
+import Foundation
+
+/// Reprensents 2D location of object
+struct Location {
+    /// Position of object on X axis
+    var x: Int
+
+    /// Position of object on Y axis
+    var y: Int
+}
