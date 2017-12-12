e7dc3103da4402dcb375a01bf50ae889394872e8
Josef Dolezal <pepik.ml@gmail.com>
2017-12-12 10:20:15 +0100

    Create logging service

---
+++ mvc-shooter/Services/CommandLineLog.swift
@@ -0,0 +1,15 @@
+//
+//  CommandLineLog.swift
+//  mvc-shooter
+//
+//  Created by Josef Dolezal on 12/12/2017.
+//  Copyright © 2017 Josef Dolezal. All rights reserved.
+//
+
+import Foundation
+
+class CommandLineLog: LogService {
+    func log(message: String) {
+        print(message)
+    }
+}
---
+++ mvc-shooter/Services/LogService.swift
@@ -0,0 +1,13 @@
+//
+//  LogService.swift
+//  mvc-shooter
+//
+//  Created by Josef Dolezal on 12/12/2017.
+//  Copyright © 2017 Josef Dolezal. All rights reserved.
+//
+
+import Foundation
+
+protocol LogService {
+    func log(message: String)
+}
